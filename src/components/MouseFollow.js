9c0dec561a4789df783717940f0e978c9d780da7
Richky Abednego <richky.abednego@gmail.com>
2025-04-11 11:06:30 +0700

    first commit

--- src/components/MouseFollow.js
+++ src/components/MouseFollow.js
@@ -1,51 +1,52 @@
-import gsap from 'gsap';
+import { gsap } from 'gsap';
 import React, { useEffect, useRef } from 'react';
 
 const MouseFollow = () => {
-   let mouseFollowRef = useRef(null);
-
-   let posX = 0
-   let posY = 0
-   let mouseX = 0
-   let mouseY = 0
-   let scrollX = 0
-   let scrollY = 0
+   const mouseFollowRef = useRef(null);
+   const posX = useRef(0);
+   const posY = useRef(0);
+   const mouseX = useRef(0);
+   const mouseY = useRef(0);
+   const scrollX = useRef(0);
+   const scrollY = useRef(0);
 
    useEffect(() => {
+      gsap.set(mouseFollowRef.current, { opacity: 0 });
+      gsap.to(mouseFollowRef.current, { opacity: 1, duration: 1 });
 
-      gsap.set(mouseFollowRef, { opacity: 0 });
-      gsap.to(mouseFollowRef, { opacity: 1, duration: 1 });
-
-      gsap.ticker.add(() => {
-         posX += (mouseX - posX) / 20;
-         posY += (mouseY - posY) / 20;
-         gsap.set(mouseFollowRef, {
+      const updatePosition = () => {
+         posX.current += (mouseX.current - posX.current) / 20;
+         posY.current += (mouseY.current - posY.current) / 20;
+         gsap.set(mouseFollowRef.current, {
             css: {
-               left: posX - 50,
-               top: posY - 50,
+               left: posX.current - 50,
+               top: posY.current - 50,
             },
          });
-      });
+      };
 
-      window.addEventListener('mousemove', (e) => {
-         scrollX = window.scrollX;
-         scrollY = window.scrollY;
-         mouseX = e.pageX - scrollX;
-         mouseY = e.pageY - scrollY;
-      });
+      gsap.ticker.add(updatePosition);
+
+      const handleMouseMove = (e) => {
+         scrollX.current = window.scrollX;
+         scrollY.current = window.scrollY;
+         mouseX.current = e.pageX - scrollX.current;
+         mouseY.current = e.pageY - scrollY.current;
+      };
+
+      window.addEventListener('mousemove', handleMouseMove);
 
       return () => {
-         gsap.ticker.remove(); // Clean up the ticker
-         window.removeEventListener('mousemove', () => { });
+         gsap.ticker.remove(updatePosition); // Clean up the ticker
+         window.removeEventListener('mousemove', handleMouseMove);
       };
    }, []);
 
-
    return (
-      <div ref={el => mouseFollowRef = el} className='fixed top-0 left-0 z-[100]'>
+      <div ref={mouseFollowRef} className='fixed top-0 left-0 z-[100]'>
          <img src="img/mouse-follow.png" alt="mouse-follow" className='size-[40px]' />
       </div>
-   )
-}
+   );
+};
 
-export default MouseFollow
+export default MouseFollow;
