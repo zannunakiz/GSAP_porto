4a1169fccb8d8a81ec1e016141cf3cc3d4f6a3f2
Richky Abednego <richky.abednego@gmail.com>
2025-04-11 11:00:07 +0700

    first commit

--- src/components/MouseFollow.js
+++ src/components/MouseFollow.js
@@ -4,7 +4,6 @@ import React, { useEffect, useRef } from 'react';
 const MouseFollow = () => {
    let mouseFollowRef = useRef(null);
 
-   let tl = gsap.timeline();
    let posX = 0
    let posY = 0
    let mouseX = 0
@@ -13,30 +12,33 @@ const MouseFollow = () => {
    let scrollY = 0
 
    useEffect(() => {
-      gsap.set(mouseFollowRef, { opacity: 0 })
-      gsap.to(mouseFollowRef, { opacity: 1, duration: 1 })
 
-      tl.to({}, 0.020, {
-         repeat: -1,
-         onRepeat: () => {
-            posX += (mouseX - posX) / 20;
-            posY += (mouseY - posY) / 20;
-            tl.set(mouseFollowRef, {
-               css: {
-                  left: posX - 50,
-                  top: posY - 50
-               }
-            })
-         }
-      })
+      gsap.set(mouseFollowRef, { opacity: 0 });
+      gsap.to(mouseFollowRef, { opacity: 1, duration: 1 });
+
+      gsap.ticker.add(() => {
+         posX += (mouseX - posX) / 20;
+         posY += (mouseY - posY) / 20;
+         gsap.set(mouseFollowRef, {
+            css: {
+               left: posX - 50,
+               top: posY - 50,
+            },
+         });
+      });
 
       window.addEventListener('mousemove', (e) => {
-         scrollX = window.scrollX
-         scrollY = window.scrollY
-         mouseX = e.pageX - scrollX
-         mouseY = e.pageY - scrollY
-      })
-   }, [])
+         scrollX = window.scrollX;
+         scrollY = window.scrollY;
+         mouseX = e.pageX - scrollX;
+         mouseY = e.pageY - scrollY;
+      });
+
+      return () => {
+         gsap.ticker.remove(); // Clean up the ticker
+         window.removeEventListener('mousemove', () => { });
+      };
+   }, []);
 
 
    return (
